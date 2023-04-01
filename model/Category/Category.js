d9afe28e2b5ff3b750a4ff5dca353b490f06dee6
Emmanuel <emmanuel@192.168.0.102>
2023-03-31 17:12:59 -0700

    Post model

---
+++ model/Category/Category.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+//schema
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    shares: {
+      type: Number,
+      default: 0,
+    },
+
+    posts: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//compile schema to  model
+
+const Category = mongoose.model("Category", categorySchema);
+
+module.exports = Category;
---
+++ model/Comment/Comment.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+//schema
+const commentSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: true,
+    },
+
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+
+    postId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//compile schema to  model
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+module.exports = Comment;
