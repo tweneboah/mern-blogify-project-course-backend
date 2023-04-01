ecb974be5363abf370912dbac0e33622bd04df6b
Emmanuel <emmanuel@192.168.0.102>
2023-03-31 22:47:38 -0700

    Error handling

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const http = require("http");
 const express = require("express");
 const usersRouter = require("./routes/users/usersRouter");
+const {
+  notFound,
+  globalErrHandler,
+} = require("./middlewares/globalErrorHandler");
 require("./config/database")();
 
 //!Server
@@ -10,26 +14,11 @@ const app = express();
 app.use(express.json()); //Pass incoming data
 // Routes
 app.use("/api/v1/users", usersRouter);
-//? Not Found middleware
-app.use((req, res, next) => {
-  const err = new Error(`Cannot find ${req.originalUrl} on the server`);
-  next(err);
-});
-//! Error middleware
-app.use((err, req, res, next) => {
-  //status
-  const status = err?.status ? err?.status : "failed";
-  //message
-  const message = err?.message;
-  //stack
-  const stack = err?.stack;
-  res.status(500).json({
-    status,
-    message,
-    stack,
-  });
-});
 
+//? Not Found middleware
+app.use(notFound);
+//! Error middleware
+app.use(globalErrHandler);
 const server = http.createServer(app);
 //? Start the server
 const PORT = process.env.PORT || 9080;
