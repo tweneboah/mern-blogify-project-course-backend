939b395ddb1f557cc2e936bed64a1afb1390c871
Emmanuel <emmanuel@Emmanuels-iMac.local>
2023-04-01 22:39:27 -0700

    get all, single, update, delete post controller

--- routes/post/postRouter.js
+++ routes/post/postRouter.js
@@ -1,5 +1,11 @@
 const express = require("express");
-const { createPost } = require("../../controllers/posts/posts");
+const {
+  createPost,
+  getPost,
+  getPosts,
+  updatePost,
+  deletePost,
+} = require("../../controllers/posts/posts");
 
 const isLoggin = require("../../middlewares/isLoggin");
 
@@ -7,5 +13,12 @@ const postsRouter = express.Router();
 
 //create
 postsRouter.post("/", isLoggin, createPost);
-
+//getting all
+postsRouter.get("/", getPosts);
+//single
+postsRouter.get("/:id", getPost);
+//update
+postsRouter.put("/:id", isLoggin, updatePost);
+//delete
+postsRouter.delete("/:id", isLoggin, deletePost);
 module.exports = postsRouter;
