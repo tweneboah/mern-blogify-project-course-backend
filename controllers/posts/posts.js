6042452ca36216b9973f4e50c444a46775285741
I-Novotek Coding Institute <emmanuel@I-Novoteks-iMac.local>
2023-04-30 17:27:52 -0700

    Hiding scheduled posts

--- controllers/posts/posts.js
+++ controllers/posts/posts.js
@@ -58,16 +58,25 @@ exports.createPost = asyncHandler(async (req, res) => {
 exports.getPosts = asyncHandler(async (req, res) => {
   // !find all users who have blocked the logged-in user
   const loggedInUserId = req.userAuth?._id;
+  //get current time
+  const currentTime = new Date();
   const usersBlockingLoggedInuser = await User.find({
     blockedUsers: loggedInUserId,
   });
   // Extract the IDs of users who have blocked the logged-in user
   const blockingUsersIds = usersBlockingLoggedInuser?.map((user) => user?._id);
 
-  const posts = await Post.find({
+  //query
+  const query = {
     author: { $nin: blockingUsersIds },
-  });
-
+    $or: [
+      {
+        shedduledPublished: { $lte: currentTime },
+        shedduledPublished: null,
+      },
+    ],
+  };
+  const posts = await Post.find(query);
   res.status(201).json({
     status: "success",
     message: "Posts successfully fetched",
